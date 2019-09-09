0de7ad8c57113649347edda8d63d8f6f2084a08f
Sebastian <sebastian.robalino@inpsercom.com>
2019-09-09 12:58:38 -0500

    campos extra en modelo empresa

--- app/Empresa.php
+++ app/Empresa.php
@@ -9,8 +9,21 @@ class Empresa extends Model
     protected $primaryKey           =       "id_em";
     protected $table                =       "empresas";
 
+    protected $appends              =       ["telp_em","tell_em"];
+
     protected $casts                =   [
         'lat_em'    =>  'double',
         'lng_em'    =>  'double'
     ];
+
+    public function getTelpEmAttribute(){
+        $nuevo  =   substr($this->attributes['telefono_em'], 2);
+        $sub    =   substr($this->attributes['telefono_em'], 1,1);
+        return "(593 $sub) ".substr_replace($nuevo," ",4,0);
+    }
+
+    public function getTellEmAttribute(){
+        $nuevo  =   substr($this->attributes['telefono_em'], 1);
+        return "+593".$nuevo;
+    }
 }
