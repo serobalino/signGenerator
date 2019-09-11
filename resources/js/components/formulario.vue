813bf5090769526bdad4481db9b4f627cc2b455b
Sebastian <sebastian.robalino@inpsercom.com>
2019-09-11 12:40:08 -0500

    bloquedo del formulario

--- resources/js/components/formulario.vue
+++ resources/js/components/formulario.vue
@@ -6,71 +6,74 @@
             <hr class="my-4">
             <p>Los campos con asterisco son obligatorios</p>
             <form @submit.prevent="validar">
-                <div class="form-group">
-                    <label><span>*</span>Nombre</label>
-                    <input type="text" v-model="formulario.nombre" name="nombre" class="form-control" placeholder="Carlos" v-validate="{required:true}"/>
-                    <small class="form-text text-danger">{{ errors.first('nombre') }}</small>
-                </div>
-                <div class="form-group">
-                    <label><span>*</span>Apellido</label>
-                    <input type="text" v-model="formulario.apellido" name="apellido" class="form-control" placeholder="Rosales" v-validate="{required:true}"/>
-                    <small class="form-text text-danger">{{ errors.first('apellido') }}</small>
-                </div>
-                <div class="form-group">
-                    <label><span>*</span>Celular</label>
-                    <input type="text" v-model="formulario.celular" name="celular" class="form-control" placeholder="0998978458" v-validate="{required:true,regex: /^[0][8-9][0-9]{7}[0-9]/,max:10}"/>
-                    <small class="form-text text-danger">{{ errors.first('celular') }}</small>
-                </div>
-                <div class="form-group">
-                    <label><span>*</span>Correo Electónico</label>
-                    <input type="email" v-model="formulario.email" name="email" class="form-control" placeholder="crosales@google.com" v-validate="{required:true}"/>
-                    <small class="form-text text-danger">{{ errors.first('email') }}</small>
-                </div>
-                <div class="form-group">
-                    <label>Extensión telefónica</label>
-                    <input type="text" v-model="formulario['extención']" name="extensión" class="form-control" placeholder="1020" />
-                    <small class="form-text text-danger">{{ errors.first('extensión') }}</small>
-                </div>
-                <div class="form-group">
-                    <label><span>*</span>Empresa</label>
-                    <select class="form-control" v-model="empresa" v-validate="{required:true}" name="empresa">
-                        <option :value="null" disabled>Selecciona</option>
-                        <option :value="item" v-for="item in empresas" :key="item.id_em">{{item.nombre_em}}</option>
-                    </select>
-                    <small class="form-text text-danger">{{ errors.first('empresa') }}</small>
-                </div>
-                <div class="form-group">
-                    <label><span>*</span>Área</label>
-                    <select class="form-control" v-model="departamento" v-validate="{required:true}" name="área">
-                        <option :value="null" disabled>Selecciona</option>
-                        <option :value="item" v-for="item in departamentos" :key="item.id_de">{{item.titulo_de}}</option>
-                    </select>
-                    <small class="form-text text-danger">{{ errors.first('área') }}</small>
-                </div>
-                <div class="form-group">
-                    <label><span>*</span>Cargo</label>
-                    <select class="form-control" v-model="formulario.cargo" v-validate="{required:true}" name="cargo">
-                        <option :value="null" disabled>Selecciona</option>
-                        <option :value="item.id_ca" v-for="item in cargos" :key="item.id_ca">{{item.nombre_ca}}</option>
-                    </select>
-                    <small class="form-text text-danger">{{ errors.first('cargo') }}</small>
-                </div>
-                <div class="form-group">
-                    <label>Skype(usuario)</label>
-                    <input type="text" v-model="formulario.skype" name="skype" class="form-control" placeholder="carlos.rosales" v-validate="{regex:/^[a-z][a-z0-9\.,\-_]{5,31}$/i}" />
-                    <small class="form-text text-danger">{{ errors.first('skype') }}</small>
-                </div>
-                <div class="form-group">
-                    <label>Github (link)</label>
-                    <input type="text" v-model="formulario.github" name="github" class="form-control" placeholder="https://github.com/crosales" v-validate="{url:true}"  />
-                    <small class="form-text text-danger">{{ errors.first('github') }}</small>
-                </div>
-                <div class="form-group">
-                    <label>LinkedIn (link)</label>
-                    <input type="text" v-model="formulario.linkedin" name="linkedin" class="form-control" placeholder="https://ec.linkedin.com/in/ricardo-rosales-18317123" v-validate="{url:true}" />
-                    <small class="form-text text-danger">{{ errors.first('linkedin') }}</small>
-                </div>
-                <button type="submit" class="btn btn-primary">Generar</button>
+                <fieldset :disabled="cargando">
+                    <div class="form-group">
+                        <label><span>*</span>Nombre</label>
+                        <input type="text" v-model="formulario.nombre" name="nombre" class="form-control" placeholder="Carlos" v-validate="{required:true}"/>
+                        <small class="form-text text-danger">{{ errors.first('nombre') }}</small>
+                    </div>
+                    <div class="form-group">
+                        <label><span>*</span>Apellido</label>
+                        <input type="text" v-model="formulario.apellido" name="apellido" class="form-control" placeholder="Rosales" v-validate="{required:true}"/>
+                        <small class="form-text text-danger">{{ errors.first('apellido') }}</small>
+                    </div>
+                    <div class="form-group">
+                        <label><span>*</span>Celular</label>
+                        <input type="text" v-model="formulario.celular" name="celular" class="form-control" placeholder="0998978458" v-validate="{required:true,regex: /^[0][8-9][0-9]{7}[0-9]/,max:10}"/>
+                        <small class="form-text text-danger">{{ errors.first('celular') }}</small>
+                    </div>
+                    <div class="form-group">
+                        <label><span>*</span>Correo Electónico</label>
+                        <input type="email" v-model="formulario.email" name="email" class="form-control" placeholder="crosales@google.com" v-validate="{required:true}"/>
+                        <small class="form-text text-danger">{{ errors.first('email') }}</small>
+                    </div>
+                    <div class="form-group">
+                        <label>Extensión telefónica</label>
+                        <input type="text" v-model="formulario['extención']" name="extensión" class="form-control" placeholder="1020" />
+                        <small class="form-text text-danger">{{ errors.first('extensión') }}</small>
+                    </div>
+                    <div class="form-group">
+                        <label><span>*</span>Empresa</label>
+                        <select class="form-control" v-model="empresa" v-validate="{required:true}" name="empresa">
+                            <option :value="null" disabled>Selecciona</option>
+                            <option :value="item" v-for="item in empresas" :key="item.id_em">{{item.nombre_em}}</option>
+                        </select>
+                        <small class="form-text text-danger">{{ errors.first('empresa') }}</small>
+                    </div>
+                    <div class="form-group">
+                        <label><span>*</span>Área</label>
+                        <select class="form-control" v-model="departamento" v-validate="{required:true}" name="área">
+                            <option :value="null" disabled>Selecciona</option>
+                            <option :value="item" v-for="item in departamentos" :key="item.id_de">{{item.titulo_de}}</option>
+                        </select>
+                        <small class="form-text text-danger">{{ errors.first('área') }}</small>
+                    </div>
+                    <div class="form-group">
+                        <label><span>*</span>Cargo</label>
+                        <select class="form-control" v-model="formulario.cargo" v-validate="{required:true}" name="cargo">
+                            <option :value="null" disabled>Selecciona</option>
+                            <option :value="item.id_ca" v-for="item in cargos" :key="item.id_ca">{{item.nombre_ca}}</option>
+                        </select>
+                        <small class="form-text text-danger">{{ errors.first('cargo') }}</small>
+                    </div>
+                    <div class="form-group">
+                        <label>Skype(usuario)</label>
+                        <input type="text" v-model="formulario.skype" name="skype" class="form-control" placeholder="carlos.rosales" v-validate="{regex:/^[a-z][a-z0-9\.,\-_]{5,31}$/i}" />
+                        <small class="form-text text-danger">{{ errors.first('skype') }}</small>
+                    </div>
+                    <div class="form-group">
+                        <label>Github (link)</label>
+                        <input type="text" v-model="formulario.github" name="github" class="form-control" placeholder="https://github.com/crosales" v-validate="{url:true}"  />
+                        <small class="form-text text-danger">{{ errors.first('github') }}</small>
+                    </div>
+                    <div class="form-group">
+                        <label>LinkedIn (link)</label>
+                        <input type="text" v-model="formulario.linkedin" name="linkedin" class="form-control" placeholder="https://ec.linkedin.com/in/ricardo-rosales-18317123" v-validate="{url:true}" />
+                        <small class="form-text text-danger">{{ errors.first('linkedin') }}</small>
+                    </div>
+                    <button type="submit" class="btn btn-primary" >Generar</button>
+                    <pre class="text-danger" v-if="cargando">{{texto}}</pre>
+                </fieldset>
             </form>
         </div>
     </div>
@@ -104,7 +107,9 @@
                 linkedin:null,
                 github:null,
                 cargo:null,
-            }
+            },
+            cargando:false,
+            texto:'Procesando',
         }),
         watch:{
             empresa:function(){
@@ -133,11 +138,11 @@
             validar:function(){
                 this.$validator.validateAll().then((result) => {
                     if (result) {
+                        this.cargando=true;
                         servicios.guardarRegistro(this.formulario).then(response=>{
+                            this.cargando=false;
                             window.location.href = response.data.data;
                         });
-                    } else {
-                        console.log("error")
                     }
                 });
             }
